0f9c56f50d04297ad5175a39da07bd55848ef870
Hufnagels <kbvconsulting@gmail.com>
2021-09-07 05:34:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  useRoutes
+} from 'react-router-dom';
+//Material
+
+//custom
+// import AuthNav from './components/Auth/AuthNav';
+import Loading from './components/Loading';
+import routes from './features/routes/routes'
+
+export default function App() {
+  const { isLoading, isAuthenticated } = useAuth0();
+  let content = useRoutes(routes);
+
+  console.log(isAuthenticated)
+  
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return content; 
+}
+
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+//import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
+//Material
+import { StyledEngineProvider } from  '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
+// custom
+import App from './App';
+import Auth0ProviderWithHistory from './components/Auth/Auth0ProviderWithHistory';
+import { store } from './redux/store/store';
+import theme from './features/theme/Theme';
+
+//const theme = createTheme();
+console.log(theme)
+const useStyles = makeStyles((theme) => {
+  
+  return {
+    root: {
+      color: theme.palette.primary.main,
+    }
+  }
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <Auth0ProviderWithHistory>
+              <App />
+            </Auth0ProviderWithHistory>
+          </Router>
+        </Provider>
+      </ThemeProvider>
+      
+    </StyledEngineProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
